87b8bf0037a16e18d11007ef234f2c68e27a2a02
aka-buccia <87319226+aka-buccia@users.noreply.github.com>
2024-08-24 11:39:31 +0200

    Aggiunto esercizio ad DB

--- src/application/resources/exercise/text/findError/es7.java
+++ src/application/resources/exercise/text/findError/es7.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.util.Random;
 
-public class es4{
+public class es7{
     public static void main(String [] args){
-        Scanner scan = new Scanner(System.in);
 
-
+        int numeroCasuale = ((Math.random() * 10) + 1);
+        System.out.println("numero generato casualmente fra 1 e 10: " + numeroCasuale);
     }
 }
